3ac367fe7e1a05bc36f5d99501d22b403f7f3c3b
Anton Alputov <anton.alputov@sharp-dev.net>
2023-08-24 21:08:32 +0400

    min max metrics

---
+++ atomic_operations/src/MinMaxMetrics.java
@@ -0,0 +1,39 @@
+public class MinMaxMetrics {
+
+    private volatile long min;
+    private volatile long max;
+
+    /**
+     * Initializes all member variables
+     */
+    public MinMaxMetrics() {
+        min = 0;
+        max = 0;
+    }
+
+    /**
+     * Adds a new sample to our metrics.
+     */
+    public synchronized void addSample(long newSample) {
+        if(newSample < min) {
+            min = newSample;
+        }
+        if(newSample > max) {
+            max = newSample;
+        }
+    }
+
+    /**
+     * Returns the smallest sample we've seen so far.
+     */
+    public long getMin() {
+        return min;
+    }
+
+    /**
+     * Returns the biggest sample we've seen so far.
+     */
+    public long getMax() {
+        return max;
+    }
+}
